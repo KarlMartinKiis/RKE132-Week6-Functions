a20036426072b5713a1ef19a14015b48f7ca7f03
Kiis <karkiis@tktk.ee>
2024-03-13 22:33:27 +0200

    add week7 code

---
+++ ArgumentsAndParamaters/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Threading.Channels;
+
+Console.WriteLine("Enter a word:");
+string UserInput = Console.ReadLine();
+
+PrintAnyWord(UserInput);
+
+static void PrintAnyWord(string anyString)
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
